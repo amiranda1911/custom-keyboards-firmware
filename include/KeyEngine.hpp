e42ea68a4d8679d28174c96d4faff07c7093b597
Anderson Miranda <anderson.js.mi@outlook.com>
2025-05-12 13:39:06 -0300

    fix: organize imports

--- include/KeyEngine.hpp
+++ include/KeyEngine.hpp
@@ -1,9 +1,19 @@
 #pragma once
-#include "Core.hpp"
+#include "core.hpp"
 
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+
+#ifdef _4X6_MATRIX_
+    const byte keyRowsCount = 4;
+    const byte keyColsCount = 6;
+#endif
+#ifdef _6X6_MATRIX_
+    const byte keyRowsCount = 6;
+    const byte keyColsCount = 6;
+
+#endif
+//#define _5X12_MATRIX_
+//#define _6X14_MATRIX_
+//#define _6X18_MATRIX_
 
 class KeyEngine
 {
@@ -11,11 +21,14 @@ class KeyEngine
 private:
     
     Adafruit_SSD1306 display;
+    bool keyState[keyRowsCount][keyColsCount];
+    byte curLayer = 1;
 
 public:
     KeyEngine();
     void update();
     void updateDisplay();
-    
-
+    void matrixKeys();
+    void keyPress(byte col, byte row);
+    void keyRelease(byte col, byte row);
 };
