5ae27f7c47e0196b6e2496696bdf2539d04ceb37
Anderson Miranda <anderson.js.mi@outlook.com>
2025-05-19 23:33:23 -0300

    feat: include HID project only avr platform

--- include/core.hpp
+++ include/core.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <Arduino.h>
-#include "HID-Project.h"
+
 
 #include <U8g2lib.h>
 #include <Wire.h>
@@ -9,15 +9,20 @@
 #include "Configuration.h"
 
 #if defined(ARDUINO_ARCH_STM32)
+    
     #pragma message "compile for STM32"
     #define EEPROM_START_ADDR 0x0801FC00
+    #include <Keyboard.h>
 
     #if defined(ARDUINO_BLUEPILL_F103C8)
         #include "PinConfig/stm32f103C8.h"
+        #include <Keyboard.h>
     #else
         #error "MCU not compatible"
     #endif
 #elif defined(__AVR__)
+    #include "HID-Project.h"
+    #include "HID-Settings.h"
     #if defined(__AVR_ATmega32U4__)
         #include "PinConfig/atmega32u4.h"
     #elif defined(ARDUINO_ARCH_SAM)
