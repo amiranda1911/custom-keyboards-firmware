226a592d35beb07c8146f4d73db4f68ea6c43a02
Anderson Miranda <anderson.js.mi@outlook.com>
2025-05-31 20:21:44 -0300

    fix: remove delays

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,11 @@ void setup ()
 {
   // inicializa o Arduino core (timers, ADC, etc.)
     keyboard = new KeyEngine();
-    delay(1000);
-
 }
 
 void loop()
 {
   keyboard->update();
-  delay(10);
+
 
 }
